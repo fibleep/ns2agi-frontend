e2fdd8db29a7529349df114a40e954e3404e51ac
fibleep <filipn924@gmail.com>
2025-01-01 10:16:13 +0100

    astro relative css issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-   site: 'https://ns2agi.com',
+	integrations: [tailwind()],
+	site: "https://ns2agi.com",
+	base: "./",
+	build: {
+		assets: "assets",
+		assetsPrefix: ".",
+	},
 });
