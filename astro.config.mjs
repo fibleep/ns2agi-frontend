07ba7df4f861166f1ae2f7cf970ccdc66ea901dd
fibleep <filipn924@gmail.com>
2025-02-06 17:23:09 +0100

    please github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,19 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import { fileURLToPath, URL } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	site: "https://ns2agi.com",
-	base: "/",
-	build: {
-		assets: "assets",
-		assetsPrefix: ".",
-	},
+  integrations: [tailwind()],
+  site: "https://ns2agi.com",
+  vite: {
+    resolve: {
+      alias: {
+        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      }
+    }
+  }
 });
+
+
